0ee65623fa885691d7b7e4e47662e72dd4f03d72
mmillan18 <2220211035@estudiantesunibague.edu.co>
2024-09-09 09:41:52 -0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\GeneradorController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Ruta inicial que muestra el número aleatorio al cargar la página
+Route::get('/', [GeneradorController::class, 'generarNumero']);
+
+// Ruta para generar un nuevo número al enviar el formulario (usando método POST)
+Route::post('/generar', [GeneradorController::class, 'generarNumero']);
